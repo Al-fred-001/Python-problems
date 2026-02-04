1edf2c8076e3ee35543fafc0af34920654a7ccdb
Alfred <infinitywriterandcreator@gmail.com>
2026-02-04 19:41:15 +0545

    Changed ignores

--- toignore/limit_of.py
+++
@@ -1,3 +0,0 @@
-# This marks the beginning of a python script. 
-
-get_user_function = input("input your function f(x): ")
--- toignore/vector_methods.py
+++
@@ -1,7 +0,0 @@
-# This marks the beginning of a python script. 
-
-
-# def vector_addition(a: int|float, b:int|float, Theta: int|float)->int|float:
-#     result =...
-#     pass
-
